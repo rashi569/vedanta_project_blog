1db3ab507f0a5c6f0069ab3c35765ed41d9f3678
rashi569 <123299048+rashi569@users.noreply.github.com>
2025-09-10 07:27:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,4 @@ def contactus():
     return render_template("contactus.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=False)
